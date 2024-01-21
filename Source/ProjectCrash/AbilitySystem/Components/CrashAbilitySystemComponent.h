686198d3673cce85538ca5874a9a2cd95405ca38
Sam Reitich <sam.reitich@gmail.com>
2024-01-21 15:35:32 -0800

    Added external link to this project's GAS framework documentation
    I would marry GAS if I could but hot damn is it difficult to understand. Even the most rudimentary implementations require an entire Google Doc to document.

--- Source/ProjectCrash/AbilitySystem/Components/CrashAbilitySystemComponent.h
+++ Source/ProjectCrash/AbilitySystem/Components/CrashAbilitySystemComponent.h
@@ -7,8 +7,13 @@
 #include "CrashAbilitySystemComponent.generated.h"
 
 /**
- * The ability system component used for this project. Contains various utilities, such as initialization methods, to
- * compartmentalize ASC functionality.
+ * The ability system component used for this project.
+ *
+ * This project's GAS implementation (like any implementation of GAS) is complex and may be difficult to understand
+ * purely from inline documentation. If you'd like a more digestible explanation of how this framework functions, view
+ * this document detailing the implementation this system from a higher level:
+ *
+ *		https://docs.google.com/document/d/1lrocajswgfGHrTl-TFuM-Iw5N444_QWDiKfILris98c/edit?usp=sharing
  */
 UCLASS()
 class PROJECTCRASH_API UCrashAbilitySystemComponent : public UAbilitySystemComponent
