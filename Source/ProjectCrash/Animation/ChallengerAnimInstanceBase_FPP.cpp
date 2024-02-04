07887f749004a61372e12f2bb8649c450559e2f9
Sam Reitich <sam.reitich@gmail.com>
2024-02-04 01:11:08 -0800

    Implemented pitch offset additives for Knight animation blueprint

--- Source/ProjectCrash/Animation/ChallengerAnimInstanceBase_FPP.cpp
+++ Source/ProjectCrash/Animation/ChallengerAnimInstanceBase_FPP.cpp
@@ -3,3 +3,62 @@
 
 #include "Animation/ChallengerAnimInstanceBase_FPP.h"
 
+#include "Camera/CameraComponent.h"
+#include "Characters/ChallengerBase.h"
+
+void UChallengerAnimInstanceBase_FPP::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// Enable multi-threading for updating animations.
+	bUseMultiThreadedAnimationUpdate = true;
+}
+
+void UChallengerAnimInstanceBase_FPP::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	// Cache the owning challenger character.
+	OwningChallenger = TryGetPawnOwner() ? Cast<AChallengerBase>(TryGetPawnOwner()) : nullptr;
+}
+
+void UChallengerAnimInstanceBase_FPP::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	UpdateAimOffset();
+
+	UpdateMovementVelocity();
+}
+
+void UChallengerAnimInstanceBase_FPP::UpdateAimOffset()
+{
+	// Cache the character's current normalized pitch.
+	if (IsValid(OwningChallenger))
+	{
+		FVector CurrentRotAsVector = OwningChallenger->GetFirstPersonCameraComponent()->GetRelativeRotation().Vector();
+		CurrentRotAsVector.Normalize();
+		NormalizedCameraPitch = CurrentRotAsVector.Z;
+	}
+}
+
+void UChallengerAnimInstanceBase_FPP::UpdateMovementVelocity()
+{
+	// Cache the character's current movement values.
+	if (IsValid(OwningChallenger))
+	{
+		SignedSpeed = OwningChallenger->GetVelocity().Length();
+	}
+}
+
+void UChallengerAnimInstanceBase_FPP::UpdateAimSpeed()
+{
+}
+
+void UChallengerAnimInstanceBase_FPP::CalculateMovementSway()
+{
+}
+
+void UChallengerAnimInstanceBase_FPP::CalculateAimSway()
+{
+}
--- Source/ProjectCrash/Animation/ChallengerAnimInstanceBase_FPP.h
+++ Source/ProjectCrash/Animation/ChallengerAnimInstanceBase_FPP.h
@@ -6,6 +6,8 @@
 #include "ChallengerAnimInstanceBase.h"
 #include "ChallengerAnimInstanceBase_FPP.generated.h"
 
+class AChallengerBase;
+
 /**
  * Base animation instance for challenger characters' first-person animation blueprints.
  */
@@ -13,5 +15,71 @@ UCLASS()
 class PROJECTCRASH_API UChallengerAnimInstanceBase_FPP : public UChallengerAnimInstanceBase
 {
 	GENERATED_BODY()
-	
+
+	// Initialization.
+
+public:
+
+	/** Default constructor. */
+	virtual void NativeInitializeAnimation() override;
+
+	/** Default initializer. */
+	virtual void NativeBeginPlay() override;
+
+
+
+	// Utils.
+
+protected:
+
+	/** This animation instance's owning pawn, cached for convenience. */
+	UPROPERTY(BlueprintReadOnly, Category = "Heroes|Animation")
+	TObjectPtr<AChallengerBase> OwningChallenger;
+
+
+
+	// Animation updating.
+
+public:
+
+	/** Thread-safe animation update function. This is used instead of NativeUpdateAnimation to utilize multi-threading,
+	 * avoiding a major bottleneck caused by traditional animation updating. This function can only call other thread-
+	 * safe functions. */
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+
+	/** Update the character's base pose using its current camera pitch. */
+	void UpdateAimOffset();
+
+	/** Update the character movement data used for animation logic. */
+	void UpdateMovementVelocity();
+
+	/** Update the character and camera rotation data used for animation logic. */
+	void UpdateAimSpeed();
+
+	/** Calculates values used to apply additive movement sway. */
+	void CalculateMovementSway();
+
+	/** Calculates values used to apply additive aim sway. */
+	void CalculateAimSway();
+
+
+
+	// Character transform values updated each animation cycle to perform animation logic.
+
+// Movement speed.
+protected:
+
+	/** The signed vector length of the character's current velocity. */
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Character Transform")
+	float SignedSpeed;
+
+// Aim & rotation.
+protected:
+
+	/** The character's camera aim rotation pitch normalized to -1.0 to 1.0, which represent looking straight down and
+	 * straight up, respectively. */
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|Character Transform")
+	float NormalizedCameraPitch = 0.0f;
 };
