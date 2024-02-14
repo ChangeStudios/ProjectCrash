649a44d2abded0736d415d84631b5bc915bba55f
Sam Reitich <sam.reitich@gmail.com>
2024-02-13 21:35:42 -0800

    Created the TriggerGameplayEvent animation notify
    There is a commonly implemented animation notify that sends a gameplay event to an actor at a certain time during an animation. This is used for ability logic that occurs at a specific point in their animation. E.g. a fireball should not be spawned until after the windup section of its animation.

---
+++ Source/ProjectCrash/Animation/AnimNotifies/AnimNotify_TriggerGameplayEvent.cpp
@@ -0,0 +1,67 @@
+// Copyright Samuel Reitich 2024.
+
+
+#include "Animation/AnimNotifies/AnimNotify_TriggerGameplayEvent.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+UAnimNotify_TriggerGameplayEvent::UAnimNotify_TriggerGameplayEvent()
+	: Super()
+{
+	// Set this notify's default color in the editor.
+#if WITH_EDITORONLY_DATA
+		NotifyColor = FColor(0, 127, 0, 255);
+#endif
+}
+
+FString UAnimNotify_TriggerGameplayEvent::GetNotifyName_Implementation() const
+{
+	// Return the name of the event tag used by this notify, if it has been set.
+	if (EventTag.IsValid())
+	{
+		return EventTag.ToString();
+	}
+
+	// Return the notify's name if an event tag has not been set.
+	return Super::GetNotifyName_Implementation();
+}
+
+void UAnimNotify_TriggerGameplayEvent::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	if (!IsValid(MeshComp) || !IsValid(MeshComp->GetOwner()))
+	{
+		// Ensure we have valid references to the executing skeletal mesh component and its owning actor.
+		if (!IsValid(MeshComp) || !IsValid(MeshComp->GetOwner()))
+		{
+			UE_LOG(LogAnimation, Warning, TEXT("UAnimNotify_TriggerGameplayEvent: Tried to send a gameplay event in animation [%s], but the skeletal mesh component and/or its owning actor are not valid."), *GetNameSafe(Animation));
+			return;
+		}
+
+		// Ensure the owner has an ASC. We can't send events to actors without an ASC.
+		UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(MeshComp->GetOwner());
+		if (!IsValid(ASC))
+		{
+			UE_LOG(LogAnimation, Warning, TEXT("UAnimNotify_TriggerGameplayEvent: Tried to send a gameplay event in animation [%s], but the owning actor, [%s], does not have a valid ASC."), *GetNameSafe(Animation), *MeshComp->GetOwner()->GetName());
+			return;
+		}
+
+		// Ensure the specified gameplay tag for the event is valid.
+		if (!EventTag.IsValid())
+		{
+			UE_LOG(LogAnimation, Warning, TEXT("UAnimNotify_TriggerGameplayEvent: Tried to send a gameplay event in animation [%s] for [%s], but the gameplay event tag is not set or is not valid."), *GetNameSafe(Animation), *MeshComp->GetOwner()->GetName());
+			return;
+		}
+	}
+
+	// Data needed to send the gameplay event.
+	FGameplayEventData EventData = FGameplayEventData();
+	EventData.Instigator = MeshComp->GetOwner();
+	EventData.EventTag = EventTag;
+
+	// Send the gameplay event to the owner of the skeletal mesh component that triggered this notify.
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(MeshComp->GetOwner(), EventTag, EventData);
+
+	Super::Notify(MeshComp, Animation, EventReference);
+}
---
+++ Source/ProjectCrash/Animation/AnimNotifies/AnimNotify_TriggerGameplayEvent.h
@@ -0,0 +1,45 @@
+// Copyright Samuel Reitich 2024.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_TriggerGameplayEvent.generated.h"
+
+/**
+ * An animation notify that triggers a gameplay event with a given gameplay event tag.
+ *
+ * The gameplay event is sent to the owner of the mesh on which this notify is triggered. This is most efficiently used
+ * in abilities with a PlayMontageAndWait followed by a WaitGameplayEventToActor node.
+ */
+UCLASS(Const, HideCategories = Object, CollapseCategories, meta = (DisplayName = "Trigger Gameplay Event"))
+class PROJECTCRASH_API UAnimNotify_TriggerGameplayEvent : public UAnimNotify
+{
+	GENERATED_BODY()
+
+	// Animation notify.
+
+public:
+
+	/** Default constructor. */
+	UAnimNotify_TriggerGameplayEvent();
+
+	/** Displays this notify's gameplay event tag as its name, if the tag is valid. */
+	virtual FString GetNotifyName_Implementation() const override;
+
+	/** Called when this notify is triggered. Sends a gameplay event to the owner of the mesh that triggered this
+	 * notify using the event tag specified by this notify. */
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+
+
+	// User-exposed parameters.
+
+public:
+
+	/** The gameplay tag used to trigger the gameplay event when this notify is triggered. The event is sent to the
+	 * owner of the mesh that triggered this notify. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimNotify", meta = (DisplayName = "Gameplay Event Tag"))
+	FGameplayTag EventTag;
+};
