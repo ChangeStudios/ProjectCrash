9879f4ca89ae1304344defef23085ed3d82d13d8
Sam Reitich <sam.reitich@gmail.com>
2024-03-09 19:05:32 -0800

    Completed implementation of the "interpolated progress bar" widget class
    This is a new widget derived from the standard progress bar that adds functionality for efficiently interpolating the displayed progress of the bar.
    
    The standard progress bar widget instantly updates to reflect its current "percent" value. The interpolated progress bar uses timers to smoothly interpolate between desired values. This widget is particularly efficient because it does not implement a "Tick" function, and only updates when necessary.

--- Source/ProjectCrash/UI/Widgets/Utils/InterpolatedProgressBar.cpp
+++ Source/ProjectCrash/UI/Widgets/Utils/InterpolatedProgressBar.cpp
@@ -3,3 +3,257 @@
 
 #include "UI/Widgets/Utils/InterpolatedProgressBar.h"
 
+UInterpolatedProgressBar::UInterpolatedProgressBar(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer),
+	CurrentInterpolationState(EInterpolationState::None),
+	CurrentInterpolationData(),
+	LastTimerTickTime(0.0f),
+	EaseOutInterpolationExponent(1.5f),
+	InterpolationUpdateInterval(0.0f)
+{
+}
+
+float UInterpolatedProgressBar::GetTargetProgressValue() const
+{
+	return IsInterpolating() ? CurrentInterpolationData.TargetValue : GetPercent();
+}
+
+void UInterpolatedProgressBar::SetCurrentProgressValue(const float NewValue)
+{
+	// Set the progress bar's progress percentage, clamped between 0.0 and 1.0.
+	SetPercent(FMath::Clamp(NewValue, 0.0f, 1.0f));
+}
+
+void UInterpolatedProgressBar::InterpolateToProgressValue(const float InTargetValue, float InMaximumInterpolationDuration, float InMinimumChangeRate, float InOutroOffset)
+{
+	const bool bWasTimerActive = TimerTickHandle.IsValid();
+
+	// Cancel any ongoing interpolation.
+	CancelInterpolation();
+
+	// Validate the given parameters.
+	ensureAlways(InMaximumInterpolationDuration > 0.0f);
+	ensureAlways(InOutroOffset <= InMaximumInterpolationDuration);
+	ensureAlways(InMinimumChangeRate >= 0.0f);
+
+	InMaximumInterpolationDuration = FMath::Max(0.0f, InMaximumInterpolationDuration);
+	InOutroOffset = FMath::Clamp(InOutroOffset, 0.0f, InMaximumInterpolationDuration);
+	InMinimumChangeRate = FMath::Max(0.0f, InMinimumChangeRate);
+
+	const float CurrentValue = GetPercent();
+
+	// Don't perform any interpolation if we are already at the target value.
+	if (InTargetValue == CurrentValue)
+	{
+		return;
+	}
+
+	// Enter the interpolation state.
+	const float InterpolationDuration = CalculateInterpolationDuration(InTargetValue, InMinimumChangeRate, InMaximumInterpolationDuration);
+	const float OutroOffset = FMath::Clamp(InOutroOffset, 0.0f, InterpolationDuration);
+	EnterInterpolation(CurrentValue, InTargetValue, InterpolationDuration, OutroOffset);
+	const bool bSuccessfullyStarted = CurrentInterpolationState != EInterpolationState::None;
+
+	// Invoke the tick timer to begin interpolation.
+	const UWorld* const World = GetWorld();
+	if (World && bSuccessfullyStarted)
+	{
+		// Update the time of the last tick if the timer wasn't already active.
+		if (!bWasTimerActive)
+		{
+			LastTimerTickTime = World->GetTimeSeconds();
+		}
+
+		// Start the tick timer. This callback will re-invoke itself with the timer until it finishes or is cancelled.
+		TimerTickHandle = World->GetTimerManager().SetTimerForNextTick(this, &UInterpolatedProgressBar::OnTimerTick);
+	}
+}
+
+bool UInterpolatedProgressBar::IsInterpolating() const
+{
+	// InterpolationState tracks when we are performing an interpolation.
+	return CurrentInterpolationState == EInterpolationState::Interpolating;
+}
+
+float UInterpolatedProgressBar::CalculateInterpolationDuration(const float InTargetValue, const float InMinimumChangeRate, const float InMaximumInterpolationDuration) const
+{
+	// Cache the progress bar's current value.
+	const float CurrentValue = GetPercent();
+
+	// Calculate the minimum change rate, taking into account which direction we're interpolating.
+	const float SignedMinimumChangeRate = InMinimumChangeRate * FMath::Sign(InTargetValue - CurrentValue);
+
+	/* Calculate the interpolation duration that is less than or equal to the maximum specified duration and changes
+	 * the progress value by at least the specified minimum rate. This formula only works if the minimum change rate is
+	 * smaller than the total required change. */
+	if (FMath::Abs(SignedMinimumChangeRate) < FMath::Abs(InTargetValue - CurrentValue))
+	{
+		/* This formula is just some math wizardry. See UCommonNumericTextBlock::CalculateInterpolationDuration for how
+		 * it's derived. */
+		const float DurationBasedOnMinimumChangeRate = 1.0f / (-FMath::Pow(-SignedMinimumChangeRate / (InTargetValue - CurrentValue) + 1.0f, 1.0f / EaseOutInterpolationExponent) + 1.0f);
+
+		return FMath::Clamp(DurationBasedOnMinimumChangeRate, 0.0f, InMaximumInterpolationDuration);
+	}
+	/* If the formula cannot be used with the given parameters, estimate how long the interpolation will take, using
+	 * the minimum change rate as the assumed change rate that will be used. */
+	else
+	{
+		return FMath::Clamp((InTargetValue - CurrentValue) / SignedMinimumChangeRate, 0.0f, InMaximumInterpolationDuration);
+	}
+}
+
+void UInterpolatedProgressBar::EnterInterpolation(const float InitialValue, const float FinalValue, const float Duration, const float OutroOffset)
+{
+	// Broadcast that this widget began an interpolation.
+	InterpolationStartedDelegate.Broadcast(this);
+
+	// Update our current interpolation state.
+	CurrentInterpolationState = EInterpolationState::Interpolating;
+
+	// Initialize the data used for the new interpolation.
+	CurrentInterpolationData.ElapsedDuration = 0.0f;
+	CurrentInterpolationData.TargetDuration = Duration;
+	CurrentInterpolationData.SourceValue = InitialValue;
+	CurrentInterpolationData.TargetValue = FinalValue;
+	CurrentInterpolationData.OutroOffset = OutroOffset;
+	CurrentInterpolationData.bHasTriggeredOutro = false;
+
+	// Perform the initial tick.
+	UpdateInterpolation();
+}
+
+void UInterpolatedProgressBar::UpdateInterpolation()
+{
+	// Cache the value before this update.
+	const float LastValue = GetPercent();
+
+	// Update the progress bar's current value (i.e. its "percent").
+	const float Alpha = FMath::Clamp(CurrentInterpolationData.ElapsedDuration / CurrentInterpolationData.TargetDuration, 0.0f, 1.0f);
+	SetPercent(FMath::InterpEaseOut(CurrentInterpolationData.SourceValue, CurrentInterpolationData.TargetValue, Alpha, EaseOutInterpolationExponent));
+
+	// Broadcast that this widget performed an interpolation update.
+	InterpolatedUpdatedDelegate.Broadcast(this, LastValue, GetPercent());
+
+	// If the outro event has not yet been triggered, check if it should be.
+	if (!CurrentInterpolationData.bHasTriggeredOutro)
+	{
+		// Trigger the outro event if we're at or past the outro point.
+		if (CurrentInterpolationData.ElapsedDuration >= CurrentInterpolationData.TargetDuration - CurrentInterpolationData.OutroOffset)
+		{
+			CurrentInterpolationData.bHasTriggeredOutro = true;
+			InterpolationOutroDelegate.Broadcast(this);
+		}
+	}
+
+	// End the interpolation once its desired duration has elapsed.
+	if (CurrentInterpolationData.ElapsedDuration >= CurrentInterpolationData.TargetDuration)
+	{
+		ExitInterpolation(true);
+	}
+}
+
+void UInterpolatedProgressBar::ExitInterpolation(const bool bHasCompleted)
+{
+	// End our current interpolation state.
+	CurrentInterpolationState = EInterpolationState::None;
+
+	// Broadcast that this widget has finished its interpolation.
+	InterpolationEndedDelegate.Broadcast(this, bHasCompleted);
+}
+
+void UInterpolatedProgressBar::CancelInterpolation()
+{
+	// Exit our current interpolation, specifying that we did not complete it.
+	if (CurrentInterpolationState == EInterpolationState::Interpolating)
+	{
+		ExitInterpolation(false);
+	}
+
+	// Cancel the current timer if it is still active.
+	if (CurrentInterpolationState == EInterpolationState::None && TimerTickHandle.IsValid())
+	{
+		if (const UWorld* const World = GetWorld())
+		{
+			World->GetTimerManager().ClearTimer(TimerTickHandle);
+		}
+	}
+}
+
+void UInterpolatedProgressBar::OnTimerTick()
+{
+	const UWorld* const World = GetWorld();
+
+	if (!World)
+	{
+		return;
+	}
+
+	// Calculate the time since the last interpolation tick (not the time since the last world tick).
+	const float CurrentTickTime = World->GetTimeSeconds();
+	const float DeltaSeconds = CurrentTickTime - LastTimerTickTime;
+
+	/* Since we can't actually perform updates at an interval of 0.0 seconds, we have to internally clamp the desired
+	 * update interval if it's too low. We arbitrarily define 60 ticks-per-second as the fastest possible update
+	 * rate. */
+	const float MinimumUpdateInterval = 0.017f;
+	const float UpdateRate = FMath::Max(InterpolationUpdateInterval, MinimumUpdateInterval);
+
+	// Fix the interpolation updates to our desired rate.
+	if (DeltaSeconds > UpdateRate)
+	{
+		// The Tick function performs the actual ticking logic.
+		Tick(DeltaSeconds);
+
+		/* Save the current time, so we know when the most recent interpolation tick was performed. This is what allows
+		 * us to use a custom tick rate. */
+		LastTimerTickTime = CurrentTickTime;
+
+		// Cancel the timer if we've stopped interpolating.
+		if (CurrentInterpolationState == EInterpolationState::None)
+		{
+			World->GetTimerManager().ClearTimer(TimerTickHandle);
+			return;
+		}
+	}
+
+	// If we haven't finished interpolating, continue ticking.
+	TimerTickHandle = World->GetTimerManager().SetTimerForNextTick(this, &UInterpolatedProgressBar::OnTimerTick);
+}
+
+void UInterpolatedProgressBar::Tick(float DeltaSeconds)
+{
+	// Don't tick if we aren't interpolating.
+	if (CurrentInterpolationState == EInterpolationState::None)
+	{
+		return;
+	}
+
+	// Don't tick when this widget is not in a valid widget tree, since it won't be rendered.
+	if (!GetCachedWidget().IsValid())
+	{
+		return;
+	}
+
+	// Update the elapsed duration of the interpolation.
+	const float TargetDuration = CurrentInterpolationData.TargetDuration;
+	const float NewElapsedDuration = CurrentInterpolationData.ElapsedDuration + DeltaSeconds;
+
+	/* If we accidentally interpolate past our target duration, cache how far over we've gone (so we can pass it onto
+	 * the next interpolation, if possible) before clamping back to the target duration. */
+	const float CurrentInterpolationOverflow = FMath::Max(0.0f, NewElapsedDuration - TargetDuration);
+	CurrentInterpolationData.ElapsedDuration = FMath::Min(NewElapsedDuration, TargetDuration);
+
+	// Perform an interpolation update.
+	const EInterpolationState PreUpdateInterpolationState = CurrentInterpolationState;
+	if (CurrentInterpolationState == EInterpolationState::Interpolating)
+	{
+		UpdateInterpolation();
+	}
+
+	/* If our interpolation was cancelled or interrupted during the update, start the next interpolation at our
+	 * overflow point, if our last interpolation overflowed. */
+	if (CurrentInterpolationState != PreUpdateInterpolationState)
+	{
+		CurrentInterpolationData.ElapsedDuration = CurrentInterpolationOverflow;
+	}
+}
--- Source/ProjectCrash/UI/Widgets/Utils/InterpolatedProgressBar.h
+++ Source/ProjectCrash/UI/Widgets/Utils/InterpolatedProgressBar.h
@@ -12,7 +12,7 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FInterpolationOutroSignature, UInter
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FInterpolationEndedSignature, UInterpolatedProgressBar*, InterpolatedProgressBar, const bool, bHasCompleted);
 
 /**
- * A progress bar which supports interpolation of its displayed value.
+ * A progress bar which supports interpolation of its displayed progress value.
  */
 UCLASS()
 class PROJECTCRASH_API UInterpolatedProgressBar : public UProgressBar
@@ -26,24 +26,21 @@ public:
     /** Default constructor. */
 	UInterpolatedProgressBar(const FObjectInitializer& ObjectInitializer);
 
-	/** Refreshes the progress bar. */
-	virtual void SynchronizeProperties() override;
-	
-	
-	
+
+
 	// Interpolation.
 	
 public:
 
 	/** Returns the progress bar value this widget will ultimately display if it is interpolating, or the current value
-	 * if it is not. Normalized from 0.0 to 1.0. */
+	 * if it is not. */
 	UFUNCTION(BlueprintCallable, Category = "Interpolated Progress Bar")
 	float GetTargetProgressValue() const;
 
 	/** Sets the current progress bar value. Cancels any ongoing interpolation. NewValue is clamped between 0.0 and
 	 * 1.0. */
 	UFUNCTION(BlueprintCallable, Category = "Interpolated Progress Bar")
-	float SetCurrentProgressValue(const float NewValue) const;
+	void SetCurrentProgressValue(const float NewValue);
 
 	/**
 	 * Starts an ongoing process of interpolating the current progress bar value to the specified target value. The
@@ -54,23 +51,18 @@ public:
 	 * @param TargetValue					The progress bar value to which to interpolate.
 	 * @param MaximumInterpolationDuration	The duration, in seconds, for the interpolation to take, at most. Must be
 	 *										greater than 0.
-	 * @param MinimumRechargeRate			The minimum change in the progress bar's value per second. Must be greater
+	 * @param MinimumChangeRate				The minimum change in the progress bar's value per second. Must be greater
 	 *										than or equal to 0.
 	 * @param OutroOffset					The amount of time, in seconds, before the end of the interpolation at
 	 *										which to trigger the outro event. Must be less than or equal to
 	 *										MaximumInterpolationDuration.
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Progress Bar Interpolation")
-	void InterpolateToProgressValue(const float TargetValue, float MaximumInterpolationDuration = 3.0f, float MinimumRechargeRate = 1.0f, float OutroOffset = 0.0f);
+	void InterpolateToProgressValue(const float TargetValue, float MaximumInterpolationDuration = 3.0f, float MinimumChangeRate = 1.0f, float OutroOffset = 0.0f);
 
 	/** Whether this widget is currently interpolating its progress value. */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Progress Bar Interpolation")
-	bool IsInterpolatingProgressValue() const;
-
-protected:
-
-	/** Wrapper for updating this progress bar's displayed progress. */
-	void UpdateDisplayedProgress();
+	bool IsInterpolating() const;
 
 // Internals.
 private:
@@ -79,54 +71,72 @@ private:
 	float CalculateInterpolationDuration(const float InTargetValue, const float InMinimumChangeRate, const float InMaximumInterpolationDuration) const;
 
 	/** Starts a new interpolation for this widget with the given parameters. */
-	void EnterProgressInterpolation(const float InitialValue, const float FinalValue, const float Duration, const float OutroOffset);
+	void EnterInterpolation(const float InitialValue, const float FinalValue, const float Duration, const float OutroOffset);
 
 	/** Ticks this widget's ongoing interpolation, if it exists. */
-	void UpdateProgressInterpolation();
+	void UpdateInterpolation();
 
 	/** Ends this widget's current interpolation, regardless of if it has finished or not. */
-	void ExitProgressInterpolation(const bool bHasCompleted = false);
+	void ExitInterpolation(const bool bHasCompleted = false);
 
 	/** Cancels any ongoing interpolation, stopping this widget's progress at its current value. */
 	void CancelInterpolation();
 
-	struct
+	/** Tracks whether we are currently interpolating this widget's progress. */
+	enum class EInterpolationState
 	{
-		float ElapsedStateDuration;
-	} InterpolationState;
+		None,
+		Interpolating
+	} CurrentInterpolationState;
 
+	/** Tracks data for the ongoing interpolation state. */
 	struct
 	{
+		float ElapsedDuration;
+		float TargetDuration;
 		float SourceValue;
 		float TargetValue;
 		float OutroOffset;
 		bool bHasTriggeredOutro;
-		float Duration;
-	} ProgressInterpolationState;
+	} CurrentInterpolationData;
 
 // Timing.
 private:
 
+	/** Primary tick function, called each tick when relevant (i.e. when an interpolation is in progress). Used as a
+	 * wrapper for Tick to implement a fixed update rate. */
 	void OnTimerTick();
-	void Tick(float DeltaTime);
 
+	/** Tick function called at a fixed rate. Invoked by OnTimerTick. */
+	void Tick(float DeltaSeconds);
+
+	/** Timer for invoking tick functions. */
 	FTimerHandle TimerTickHandle;
+
+	/** Tracks how long it's been since we performed a Tick update (not how long it's been between world ticks). Used
+	 * to implement fixed tick rates. */
 	float LastTimerTickTime;
+
 
 
 	// Interpolation event delegates.
 
 public:
 
+	/** Fired when an interpolation begins. */
 	UPROPERTY(BlueprintAssignable, Category = "Progress Bar Interpolation", DisplayName = "On Interpolation Started")
 	FInterpolationStartedSignature InterpolationStartedDelegate;
 
+	/** Fired each interpolation update (each tick through an interpolation). */
 	UPROPERTY(BlueprintAssignable, Category = "Progress Bar Interpolation", DisplayName = "On Interpolation Updated")
 	FInterpolationUpdatedSignature InterpolatedUpdatedDelegate;
 
+	/** Fired when the interpolation outro begins. This can occur before the interpolation ends if desired. */
 	UPROPERTY(BlueprintAssignable, Category = "Progress Bar Interpolation", DisplayName = "On Outro Event")
 	FInterpolationOutroSignature InterpolationOutroDelegate;
 
+	/** Fired when the interpolation completely ends. This is fired whether the interpolation successfully completed
+	 * or if it was ended prematurely. */
 	UPROPERTY(BlueprintAssignable, Category = "Progress Bar Interpolation", DisplayName = "On Interpolation Ended")
 	FInterpolationEndedSignature InterpolationEndedDelegate;
 
@@ -136,13 +146,12 @@ public:
 
 public:
 
-	/** Exponent parameter for the "ease out" interpolation curve. Must be > 0, but should be > 1 in order to "ease out". */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Numeric Interpolation", meta = (ClampMin = "1.0"))
+	/** Exponent parameter for the "ease out" interpolation curve. Must be > 1.0. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Progress Bar Interpolation", meta = (ClampMin = "1.0"))
 	float EaseOutInterpolationExponent;
 
-	/** The desired interval, in seconds, between interpolation updates. 0.0 implies per-frame updates. NOTE: Interpolation updates may occur further apart due to tick rates. */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Numeric Interpolation", meta = (ClampMin = "0.0"))
+	/** The desired interval, in seconds, between interpolation updates. 0.0 implies per-frame updates. Note that
+	 * interpolation updates may occur further apart than this due to tick rates. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Progress Bar Interpolation", meta = (ClampMin = "0.0"))
 	float InterpolationUpdateInterval;
-
-
 };
