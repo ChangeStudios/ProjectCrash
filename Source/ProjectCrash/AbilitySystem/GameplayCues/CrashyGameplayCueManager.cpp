cf4746ea955a1977a5dd985773ea0d6feff24365
Sam Reitich <sam.reitich@gmail.com>
2024-04-29 19:50:16 -0700

    Packaged v0.2.1
    Made small tweaks and fixes for packaging. Also fixed a bug preventing gameplay cues from being properly loaded in packaged builds (for whatever reason, they HAVE to be added into the "DirectoriesToAlwaysCook" setting, even if their path is specified in GAS globals).

--- Source/ProjectCrash/AbilitySystem/GameplayCues/CrashyGameplayCueManager.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Samuel Reitich 2024.
-
-
-#include "AbilitySystem/GameplayCues/CrashyGameplayCueManager.h"
-
-bool UCrashyGameplayCueManager::ShouldAsyncLoadRuntimeObjectLibraries() const
-{
-	return false;
-}
