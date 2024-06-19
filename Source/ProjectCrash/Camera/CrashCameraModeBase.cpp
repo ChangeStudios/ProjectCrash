ee02f7eec796cf0f21ce612599de04410a045185
Sam Reitich <sam.reitich@gmail.com>
2024-06-19 16:57:12 -0700

    Created the CameraModeStack class
    This class will be used by the CrashCameraComponent to manage switching between multiple camera modes.
    
    For example, an ability may want to change the camera (think of Reinhardt's shield, supers in Destiny, etc.). It can push its desired camera mode to this stack, the camera will blend to it, then when the ability ends, it can pop the camera mode from the stack, and the camera will return to its previous camera.
    
    This is very similar to the ChallengerBase class's camera stack system. However, the Challenger only came with two camera modes: first-person and third-person, and did not support modular addition of new cameras.

--- Source/ProjectCrash/Camera/CrashCameraModeBase.cpp
+++ Source/ProjectCrash/Camera/CrashCameraModeBase.cpp
@@ -5,6 +5,7 @@
 
 #include "CrashCameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Engine/Canvas.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CrashLogging.h"
 
@@ -49,6 +50,8 @@ void FCrashCameraModeView::Blend(const FCrashCameraModeView& Other, float Weight
 	FieldOfView = FMath::Lerp(FieldOfView, Other.FieldOfView, Weight);
 }
 
+
+
 /**
  * UCrashCameraModeBase
  */
@@ -72,7 +75,7 @@ void UCrashCameraModeBase::UpdateCameraMode(float DeltaTime)
 	// Update our current view.
 	UpdateView(DeltaTime);
 
-	// Progress any ongoing interpolation
+	// Progress any ongoing blending
 	UpdateBlending(DeltaTime);
 }
 
@@ -142,6 +145,40 @@ void UCrashCameraModeBase::UpdateView(float DeltaTime)
 	View.FieldOfView = FieldOfView;
 }
 
+void UCrashCameraModeBase::SetBlendWeight(float Weight)
+{
+	BlendWeight = FMath::Clamp(Weight, 0.0f, 1.0f);
+
+	// Clamp exponent above 0.0. Default to 1.0.
+	const float Exponent = (BlendExponent > 0.0f) ? BlendExponent : 1.0f;
+	const float InvExponent = 1.0f / Exponent;
+
+	/* Recalculate the blend alpha. We normally calculate the weight from the alpha. But since we're setting the weight
+	 * directly, we need to use an inverse exponent to calculate the alpha. */
+	switch (BlendFunction)
+	{
+		case ECrashCameraModeBlendFunction::Linear:
+			BlendAlpha = BlendWeight;
+			break;
+
+		case ECrashCameraModeBlendFunction::EaseIn:
+			BlendAlpha = FMath::InterpEaseIn(0.0f, 1.0f, BlendWeight, InvExponent);
+			break;
+
+		case ECrashCameraModeBlendFunction::EaseOut:
+			BlendAlpha = FMath::InterpEaseOut(0.0f, 1.0f, BlendWeight, InvExponent);
+			break;
+
+		case ECrashCameraModeBlendFunction::EaseInOut:
+			BlendAlpha = FMath::InterpEaseInOut(0.0f, 1.0f, BlendWeight, InvExponent);
+			break;
+
+		default:
+			checkf(false, TEXT("Invalid blend function [%d] used for camera mode [%s]."), (uint8)BlendFunction, *GetNameSafe(this));
+			break;
+	}
+}
+
 void UCrashCameraModeBase::UpdateBlending(float DeltaTime)
 {
 	// Calculate current blend alpha.
@@ -201,6 +238,17 @@ AActor* UCrashCameraModeBase::GetTargetActor() const
 	return GetCrashCameraComponent()->GetTargetActor();
 }
 
+void UCrashCameraModeBase::DrawDebug(UCanvas* Canvas) const
+{
+	check(Canvas);
+
+	FDisplayDebugManager& DisplayDebugManager = Canvas->DisplayDebugManager;
+
+	// Display this camera mode's name and current weight.
+	DisplayDebugManager.SetDrawColor(FColor::White);
+	DisplayDebugManager.DrawString(FString::Printf(TEXT("			Camera Mode: %s (%f)"), *GetName(), BlendWeight));
+}
+
 #if WITH_EDITOR
 void UCrashCameraModeBase::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
 {
@@ -244,4 +292,248 @@ void UCrashCameraModeBase::PostEditChangeProperty(FPropertyChangedEvent& Propert
 		}
 	}
 }
-#endif
+#endif // WITH_EDITOR
+
+
+
+/**
+ * UCrashCameraModeStack
+ */
+UCrashCameraModeStack::UCrashCameraModeStack()
+{
+	// Stacks start as active. There should be no camera modes at this point.
+	bIsActive = true;
+}
+
+void UCrashCameraModeStack::ActivateStack()
+{
+	if (!bIsActive)
+	{
+		bIsActive = true;
+
+		// Notify all camera modes on the stack of activation.
+		for (UCrashCameraModeBase* CameraMode : CameraModeStack)
+		{
+			check(CameraMode);
+			CameraMode->OnActivation();
+		}
+	}
+}
+
+void UCrashCameraModeStack::DeactivateStack()
+{
+	if (bIsActive)
+	{
+		bIsActive = false;
+
+		// Notify all camera modes on the stack of deactivation.
+		for (UCrashCameraModeBase* CameraMode : CameraModeStack)
+		{
+			check(CameraMode);
+			CameraMode->OnDeactivation();
+		}
+	}
+}
+
+void UCrashCameraModeStack::PushCameraMode(TSubclassOf<UCrashCameraModeBase> CameraModeClass)
+{
+	if (!ensure(CameraModeClass))
+	{
+		return;
+	}
+
+	// Create a new camera mode instance of the specified type, or retrieve the instance if it already exists.
+	UCrashCameraModeBase* CameraMode = GetCameraModeInstance(CameraModeClass);
+	check(CameraMode);
+
+
+	// If the new camera mode is already at the top of the stack, we don't have to do anything else.
+	int32 StackSize = CameraModeStack.Num();
+	if ((StackSize > 0) && (CameraModeStack[0] == CameraMode))
+	{
+		return;
+	}
+
+
+	// Check if the new camera mode is already in the camera stack.
+	int32 ExistingStackIndex = INDEX_NONE;
+	float ExistingStackContribution = 1.0f;
+
+	for (int32 StackIndex = 0; StackIndex < StackSize; StackIndex++)
+	{
+		if (CameraModeStack[StackIndex] == CameraMode)
+		{
+			ExistingStackIndex = StackIndex;
+			ExistingStackContribution *= CameraMode->GetBlendWeight();
+			break;
+		}
+		else
+		{
+			ExistingStackContribution *= (1.0f - CameraModeStack[StackIndex]->GetBlendWeight());
+		}
+	}
+
+	// If the camera mode is already in the stack, remove its existing instance. Keep the new one.
+	if (ExistingStackIndex != INDEX_NONE)
+	{
+		CameraModeStack.RemoveAt(ExistingStackIndex);
+		StackSize--;
+	}
+	else
+	{
+		ExistingStackContribution = 0.0f;
+	}
+
+
+	/* Determine the weight with which to start blending the new camera mode. The camera mode should only blend if it
+	 * has a blend time set and there's an existing camera mode from which to blend (i.e. this isn't the first camera in
+	 * the stack). If the camera mode was already in the stack, start blending from where that existing camera  was. */ 
+	const bool bShouldBlend = (CameraMode->GetBlendTime() > 0.0f) && (StackSize > 0);
+	const float BlendWeight = (bShouldBlend ? ExistingStackContribution : 1.0f);
+
+	CameraMode->SetBlendWeight(BlendWeight);
+
+	// Add the camera mode to the top of the stack.
+	CameraModeStack.Insert(CameraMode, 0);
+
+	// Make sure the bottom of the stack is always weight to 1.0.
+	CameraModeStack.Last()->SetBlendWeight(1.0f);
+
+	// Notify the camera mode that it was added to the stack, if it wasn't already in it.
+	if (ExistingStackIndex == INDEX_NONE)
+	{
+		CameraMode->OnActivation();
+	}
+}	
+
+bool UCrashCameraModeStack::EvaluateStack(float DeltaTime, FCrashCameraModeView& OutCameraModeView)
+{
+	// Don't evaluate inactive camera stacks.
+	if (!bIsActive)
+	{
+		return false;
+	}
+
+	// Re-evaluate the current camera mode.
+	UpdateStack(DeltaTime);
+
+	// Progress any ongoing blending.
+	BlendStack(OutCameraModeView);
+
+	return true;
+}
+
+void UCrashCameraModeStack::DrawDebug(UCanvas* Canvas) const
+{
+	check(Canvas);
+
+	FDisplayDebugManager& DisplayDebugManager = Canvas->DisplayDebugManager;
+
+	DisplayDebugManager.SetDrawColor(FColor::Green);
+	DisplayDebugManager.DrawString(FString(TEXT("	--- Camera Modes (Begin) ---")));
+
+	// Draw debug information for each camera mode in this stack.
+	for (const UCrashCameraModeBase* CameraMode : CameraModeStack)
+	{
+		check(CameraMode);
+		CameraMode->DrawDebug(Canvas);
+	}
+
+	DisplayDebugManager.SetDrawColor(FColor::Green);
+	DisplayDebugManager.DrawString(FString(TEXT("	--- Camera Modes (End) ---")));
+}
+
+UCrashCameraModeBase* UCrashCameraModeStack::GetCameraModeInstance(TSubclassOf<UCrashCameraModeBase> CameraModeClass)
+{
+	check(CameraModeClass);
+
+	// Search for an existing camera mode instance.
+	for (UCrashCameraModeBase* CameraMode : CameraModeInstances)
+	{
+		if ((CameraMode != nullptr) && (CameraMode->GetClass() == CameraModeClass))
+		{
+			return CameraMode;
+		}
+	}
+
+	// If a camera mode instance of the specified type does not exist yet, create one.
+	UCrashCameraModeBase* NewCameraMode = NewObject<UCrashCameraModeBase>(GetOuter(), CameraModeClass, NAME_None, RF_NoFlags);
+	check(NewCameraMode);
+
+	// Cache the new camera mode instance.
+	CameraModeInstances.Add(NewCameraMode);
+
+	return NewCameraMode;
+}
+
+void UCrashCameraModeStack::UpdateStack(float DeltaTime)
+{
+	// No camera modes.
+	const int32 StackSize = CameraModeStack.Num();
+	if (StackSize <= 0)
+	{
+		return;
+	}
+
+
+	int32 RemoveCount = 0;
+	int32 RemoveIndex = INDEX_NONE;
+
+	// Update each camera mode.
+	for (int32 StackIndex = 0; StackIndex < StackSize; StackIndex++)
+	{
+		UCrashCameraModeBase* CameraMode = CameraModeStack[StackIndex];
+		check(CameraMode);
+
+		CameraMode->UpdateCameraMode(DeltaTime);
+
+		/* If we have a camera mode with a blend weight of 1.0, that should be our currently viewed camera mode.
+		 * Everything below it is irrelevant and can be removed. */
+		if (CameraMode->GetBlendWeight() >= 1.0f)
+		{
+			RemoveIndex = (StackIndex + 1);
+			RemoveCount = (StackSize - RemoveIndex);
+		}
+	}
+
+
+	// Remove any camera modes that are now irrelevant.
+	if (RemoveCount > 0)
+	{
+		// Notify the camera modes of deactivation.
+		for (int32 StackIndex = RemoveIndex; StackIndex < RemoveIndex; StackIndex++)
+		{
+			UCrashCameraModeBase* CameraMode = CameraModeStack[StackIndex];
+			check(CameraMode);
+
+			CameraMode->OnDeactivation();
+		}
+
+		// Remove the camera mode from the stack.
+		CameraModeStack.RemoveAt(RemoveIndex);
+	}
+}
+
+void UCrashCameraModeStack::BlendStack(FCrashCameraModeView& OutCameraModeView) const
+{
+	// No camera modes.
+	const int32 StackSize = CameraModeStack.Num();
+	if (StackSize <= 0)
+	{
+		return;
+	}
+
+	// Start with the bottom camera mode.
+	const UCrashCameraModeBase* CameraMode = CameraModeStack[StackSize - 1];
+	check(CameraMode);
+	OutCameraModeView = CameraMode->GetCameraModeView();
+
+	// Blend up the stack.
+	for (int32 StackIndex = (StackSize - 2); StackIndex >= 0; StackIndex--)
+	{
+		CameraMode = CameraModeStack[StackIndex];
+		check(CameraMode);
+
+		OutCameraModeView.Blend(CameraMode->GetCameraModeView(), CameraMode->GetBlendWeight());
+	}
+}
--- Source/ProjectCrash/Camera/CrashCameraModeBase.h
+++ Source/ProjectCrash/Camera/CrashCameraModeBase.h
@@ -8,9 +8,7 @@
 class UCrashCameraComponent;
 
 /**
- * ECrashCameraModeBlendFunction
- *
- *  Blend function used for transitioning between camera modes.
+ * Blend function used for transitioning between camera modes.
  */
 UENUM(BlueprintType)
 enum class ECrashCameraModeBlendFunction : uint8
@@ -78,10 +76,15 @@ public:
 
 public:
 
-	/** Called continuously to update the camera's current view and to handle transitioning between camera modes. */
+	/** Continuously called to update the camera's current view and to handle transitioning between camera modes. */
 	void UpdateCameraMode(float DeltaTime);
 
-	/** Called when this camera mode is activated via the camera stack. */
+	/**
+	 * Called when this camera mode is activated via the camera stack.
+	 *
+	 * This is only called when a new camera mode is activated. If a camera mode is added to a stack that already had
+	 * an instance of that camera mode, this function is not called.
+	 */
 	virtual void OnActivation() {}
 
 	/** Called when this camera mode is deactivated via the camera stack. */
@@ -90,6 +93,11 @@ public:
 
 
 	// Camera view.
+
+public:
+
+	/** Retrieves this camera mode's current view. */
+	FORCEINLINE const FCrashCameraModeView& GetCameraModeView() const { return View; }
 
 protected:
 
@@ -146,6 +154,18 @@ protected:
 
 	// Blending.
 
+public:
+
+	/** This camera mode's desired blend time. */
+	FORCEINLINE float GetBlendTime() const { return BlendTime; }
+
+	/** The current blend weight of this camera mode. 1.0 if this is camera mode is the current focus. 0.0 if this
+	 * camera mode is not being viewed. */
+	FORCEINLINE float GetBlendWeight() const { return BlendWeight; }
+
+	/** Directly updates the current blend weight. Updates the blend alpha to match. */
+	void SetBlendWeight(float Weight);
+
 protected:
 
 	/** Updates this camera mode's current blending values. */
@@ -187,6 +207,9 @@ public:
 	/** Helper for retrieving the outer camera component's target actor. */
 	AActor* GetTargetActor() const;
 
+	/** Displays information about this camera mode. */
+	virtual void DrawDebug(UCanvas* Canvas) const;
+
 #if WITH_EDITOR
 	/** Prevents Min and Max clamps from passing each other. */
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
@@ -194,9 +217,76 @@ public:
 };
 
 
+
+/**
+ * A stack of camera modes that can be used to blend between them as they are pushed and popped.
+ */
 UCLASS()
 class UCrashCameraModeStack : public UObject
 {
 	GENERATED_BODY()
 
+	// Construction.
+
+public:
+
+	/** Default constructor. */
+	UCrashCameraModeStack();
+
+
+
+	// Initialization.
+
+public:
+
+	/** Activates this stack, allowing its cameras to be evaluated. Stacks are active by default, so this should only be
+	 * called on stacks that have been deactivated. Stacks begin as active. */
+	void ActivateStack();
+
+	/** Deactivates this stack, preventing its cameras from being used. */
+	void DeactivateStack();
+
+	/** Returns whether this stack is currently active. */
+	FORCEINLINE bool IsStackActivated() const { return bIsActive; }
+
+protected:
+
+	/** Whether this stack is currently active. Cameras are not evaluated while the stack is inactive. */
+	bool bIsActive;
+
+
+
+	// Camera stack.
+
+public:
+
+	/** Creates a new camera mode instance of the specified type and pushes it to the camera mode stack. */
+	void PushCameraMode(TSubclassOf<UCrashCameraModeBase> CameraModeClass);
+
+	/** Continuously called to update this stack's current camera mode and any active blending. */
+	bool EvaluateStack(float DeltaTime, FCrashCameraModeView& OutCameraModeView);
+
+	/** Draws debug information about this camera stack's current camera modes. */
+	void DrawDebug(UCanvas* Canvas) const;
+
+protected:
+
+	/** Returns the camera mode instance of the specified type in this camera stack. Creates a new one if one does not
+	 * exist yet. */
+	UCrashCameraModeBase* GetCameraModeInstance(TSubclassOf<UCrashCameraModeBase> CameraModeClass);
+
+	/** Updates each camera mode on the stack. Removes any camera modes that are now irrelevant. */
+	void UpdateStack(float DeltaTime);
+
+	/** Blends up the stack, between each weighted camera mode. */
+	void BlendStack(FCrashCameraModeView& OutCameraModeView) const;
+
+	/** The internal ordered stack of camera mode instances. */
+	UPROPERTY()
+	TArray<TObjectPtr<UCrashCameraModeBase>> CameraModeStack;
+
+	/** Unordered collection of the camera mode instances created by this stack. Maintains references to camera mode
+	 * instances as they're added to and removed from the stack. */
+	UPROPERTY()
+	TArray<TObjectPtr<UCrashCameraModeBase>> CameraModeInstances;
 };
