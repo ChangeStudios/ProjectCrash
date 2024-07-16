2155a8468a8bd95f34ddf84b82e670230b8184a2
Sam Reitich <sam.reitich@gmail.com>
2024-07-16 00:14:28 -0700

    Created outline for third-person character animation instance

--- Source/ProjectCrash/Animation/ThirdPersonCharacterAnimInstance.cpp
+++ Source/ProjectCrash/Animation/ThirdPersonCharacterAnimInstance.cpp
@@ -2,3 +2,22 @@
 
 
 #include "Animation/ThirdPersonCharacterAnimInstance.h"
+
+void UThirdPersonCharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+}
+
+void UThirdPersonCharacterAnimInstance::UpdateVelocityData(float DeltaSeconds)
+{
+	Super::UpdateVelocityData(DeltaSeconds);
+
+	// Determine which local cardinal direction this character is currently moving.
+	LocalVelocityDirection = SelectCardinalDirectionFromAngle(LocalVelocityDirectionAngle, 0.0f, true, LocalVelocityDirection);
+}
+
+EAnimCardinalDirection UThirdPersonCharacterAnimInstance::SelectCardinalDirectionFromAngle(float LocalAngle, float DeadZone, bool bUseCurrentDirection, EAnimCardinalDirection CurrentDirection)
+{
+	return EAnimCardinalDirection::Forward;
+}
+
--- Source/ProjectCrash/Animation/ThirdPersonCharacterAnimInstance.h
+++ Source/ProjectCrash/Animation/ThirdPersonCharacterAnimInstance.h
@@ -6,14 +6,17 @@
 #include "Animation/CharacterAnimInstanceBase.h"
 #include "ThirdPersonCharacterAnimInstance.generated.h"
 
-/** Represents one of four cardinal directions. Used for selecting 4D locomotion animations. */
+/**
+ * Represents one of four cardinal directions relative to the character's rotation. Used for selecting 4D locomotion
+ * animations.
+ */
 UENUM(BlueprintType)
-enum FCardinalDirection : uint8
+enum class EAnimCardinalDirection : uint8
 {
-	North,
-	South,
-	East,
-	West
+	Forward,
+	Backward,
+	Right,
+	Left
 };
 
 
@@ -26,4 +29,50 @@ UCLASS(Abstract)
 class PROJECTCRASH_API UThirdPersonCharacterAnimInstance : public UCharacterAnimInstanceBase
 {
 	GENERATED_BODY()
+
+	// Animation updates.
+
+public:
+
+	/** Collects additional data used to drive third-person animations. */
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+
+	/** Collects additional character velocity data for third-person animations. */
+	virtual void UpdateVelocityData(float DeltaSeconds) override;
+
+
+
+	// Animation data.
+
+// Velocity data.
+protected:
+
+	/** The cardinal direction of this character current local velocity. */
+	UPROPERTY(BlueprintReadOnly, Category = "Velocity Data")
+	EAnimCardinalDirection LocalVelocityDirection;
+
+
+
+	// Utils.
+
+private:
+
+	/**
+	 * Converts a given direction, represented by an angle, into a cardinal direction.
+	 *
+	 * @param LocalAngle				The direction which will be converted to a cardinal direction.
+	 * @param DeadZone					Leniency for "Forward" and "Backward" directions. Used to increase the range in
+	 *									which "Forward" or "Backward" will be selected, for when these directions should
+	 *									be favored.
+	 * @param bUseCurrentDirection		If true, the given DeadZone will be increased if already moving forward or
+	 *									backward, making it more difficult to change directions and preventing rapid
+	 *									changes.
+	 * @param CurrentDirection			The current cardinal direction, used for skewing the dead-zone if
+	 *									bUseCurrentDirection is true.
+	 * @return							The given direction as a cardinal direction, taking into account any given
+	 *									dead-zones.
+	 */
+	static EAnimCardinalDirection SelectCardinalDirectionFromAngle(float LocalAngle, float DeadZone, bool bUseCurrentDirection, EAnimCardinalDirection CurrentDirection);
 };
