1f25617bc112ba57848ba2ab7a2f2c0fefefb2c7
Sam Reitich <sam.reitich@gmail.com>
2024-07-28 12:29:38 -0700

    Added asset types for the team system
    The team system's editor assets (TeamCreationComponent and TeamDisplayAsset) now have custom asset types.

---
+++ Source/ProjectCrashEditor/Private/AssetTypes/AssetTypeActions_Teams.cpp
@@ -0,0 +1,56 @@
+// Copyright Samuel Reitich. All rights reserved.
+
+
+#include "AssetTypes/AssetTypeActions_Teams.h"
+
+#include "GameFramework/Teams/TeamCreationComponent.h"
+#include "GameFramework/Teams/TeamDisplayAsset.h"
+
+#define LOCTEXT_NAMESPACE "AssetTypeActions"
+
+/**
+ * FAssetTypeActions_TeamCreationComponent
+ */
+FText FAssetTypeActions_TeamCreationComponent::GetName() const
+{
+	return NSLOCTEXT("AssetTypeActions", "AssetTypeActions_TeamCreationComponent", "Team Creation Component");
+}
+
+FColor FAssetTypeActions_TeamCreationComponent::GetTypeColor() const
+{
+	return FColor(0, 0, 255);
+}
+
+UClass* FAssetTypeActions_TeamCreationComponent::GetSupportedClass() const
+{
+	return UTeamCreationComponent::StaticClass();
+}
+
+uint32 FAssetTypeActions_TeamCreationComponent::GetCategories()
+{
+	return EAssetTypeCategories::Gameplay;
+}
+
+
+/**
+ * FAssetTypeActions_TeamDisplayAsset
+ */
+FText FAssetTypeActions_TeamDisplayAsset::GetName() const
+{
+	return NSLOCTEXT("AssetTypeActions", "AssetTypeActions_TeamDisplayAsset", "Team Display Asset");
+}
+
+FColor FAssetTypeActions_TeamDisplayAsset::GetTypeColor() const
+{
+	return FColor(0, 255, 255);
+}
+
+UClass* FAssetTypeActions_TeamDisplayAsset::GetSupportedClass() const
+{
+	return UTeamDisplayAsset::StaticClass();
+}
+
+uint32 FAssetTypeActions_TeamDisplayAsset::GetCategories()
+{
+	return EAssetTypeCategories::Gameplay;
+}
---
+++ Source/ProjectCrashEditor/Public/AssetTypes/AssetTypeActions_Teams.h
@@ -0,0 +1,36 @@
+// Copyright Samuel Reitich. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetTypeActions_Base.h"
+
+/**
+ * UTeamCreationComponent asset type.
+ */
+class FAssetTypeActions_TeamCreationComponent : public FAssetTypeActions_Base
+{
+public:
+	// IAssetTypeActions Implementation
+	virtual FText GetName() const override;
+	virtual FColor GetTypeColor() const override;
+	virtual UClass* GetSupportedClass() const override;
+	virtual uint32 GetCategories() override;
+	// End IAssetTypeActions Implementation
+};
+
+
+
+/**
+ * UTeamDisplayAsset asset type.
+ */
+class FAssetTypeActions_TeamDisplayAsset : public FAssetTypeActions_Base
+{
+public:
+	// IAssetTypeActions Implementation
+	virtual FText GetName() const override;
+	virtual FColor GetTypeColor() const override;
+	virtual UClass* GetSupportedClass() const override;
+	virtual uint32 GetCategories() override;
+	// End IAssetTypeActions Implementation
+};
