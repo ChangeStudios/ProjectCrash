ea24babafccce85527484b2ff6b21bfcb5df65d1
Sam Reitich <sam.reitich@gmail.com>
2024-08-01 12:55:45 -0700

    Implemented the CrashHUDLayout widget
    This is a widget that defines the modular layout of widgets in a HUD using "extension points." Once a HUD layout is pushed, widgets can be inserted into its extension points, either via the "Add Widgets" game feature action or the "Register Extension" functions in the UI Extension subsystem.

---
+++ Source/ProjectCrash/UI/CrashHUDLayout.cpp
@@ -0,0 +1,32 @@
+// Copyright Samuel Reitich. All rights reserved.
+
+
+#include "UI/CrashHUDLayout.h"
+
+#include "CommonUIExtensions.h"
+#include "CrashGameplayTags.h"
+#include "Input/CommonUIInputTypes.h"
+#include "NativeGameplayTags.h"
+#include "UI/Deprecated/CrashBoundActionButton.h"
+
+UCrashHUDLayout::UCrashHUDLayout(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UCrashHUDLayout::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	// Register the "escape" action.
+	RegisterUIActionBinding(FBindUIActionArgs(FUIActionTag::ConvertChecked(CrashGameplayTags::TAG_UI_Action_Escape), false, FSimpleDelegate::CreateUObject(this, &UCrashHUDLayout::HandleEscapeAction)));
+}
+
+void UCrashHUDLayout::HandleEscapeAction()
+{
+	// Push the escape menu.
+	if (ensure(!EscapeMenuClass.IsNull()))
+	{
+		UCommonUIExtensions::PushStreamedContentToLayer_ForPlayer(GetOwningLocalPlayer(), CrashGameplayTags::TAG_UI_Action_Escape, EscapeMenuClass);
+	}
+}
---
+++ Source/ProjectCrash/UI/CrashHUDLayout.h
@@ -0,0 +1,39 @@
+// Copyright Samuel Reitich. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Deprecated/Framework/CrashActivatableWidget.h"
+#include "CrashHUDLayout.generated.h"
+
+/**
+ * A widget that defines the layout of an in-game HUD using extension points. Widgets can be inserted into extension
+ * points, either via the "Add Widgets" game feature action or the "Register Extension" functions in the UI Extension
+ * Subsystem.
+ *
+ * HUD layouts should rarely contain actual widgets and should never implement any logic.
+ *
+ * This class also provides an escape menu (e.g. an in-game pause menu) using an "Escape" Common UI action.
+ */
+UCLASS(Abstract, BlueprintType, Blueprintable)
+class PROJECTCRASH_API UCrashHUDLayout : public UCrashActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	/** Default constructor. */
+	UCrashHUDLayout(const FObjectInitializer& ObjectInitializer);
+
+	/** Registers the "escape" action to push the escape menu to the "Menu" layer. */
+	virtual void NativeOnInitialized() override;
+
+protected:
+
+	/** Pushes the specified escape menu widget to the local player. */
+	void HandleEscapeAction();
+
+	/** The widget to push to the owning player when the "escape" action is triggered. */
+	UPROPERTY(EditDefaultsOnly)
+	TSoftClassPtr<UCommonActivatableWidget> EscapeMenuClass;
+};
