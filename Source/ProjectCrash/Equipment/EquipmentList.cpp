cdcfd3758da595f51a6040f3361833a24dad5a40
Sam Reitich <sam.reitich@gmail.com>
2024-08-12 11:40:06 -0700

    Implemented the EquipmentList
    This is a fast-serialized array that will track a pawn's current equipment. Only equipment that is currently equipped is tracked. When equipment is unequipped, it will be destroyed.

--- Source/ProjectCrash/Equipment/EquipmentList.cpp
+++ Source/ProjectCrash/Equipment/EquipmentList.cpp
@@ -2,3 +2,149 @@
 
 
 #include "Equipment/EquipmentList.h"
+
+#include "EquipmentActor.h"
+#include "EquipmentDefinition.h"
+#include "EquipmentInstance.h"
+#include "EquipmentSkin.h"
+#include "AbilitySystem/CrashAbilitySystemGlobals.h"
+#include "AbilitySystem/Components/CrashAbilitySystemComponent.h"
+#include "GameFramework/CrashLogging.h"
+
+/**
+ * FEquipmentListEntry
+ */
+FString FEquipmentListEntry::GetDebugString() const
+{
+    return FString::Printf(TEXT("%s (%s)"), *GetNameSafe(EquipmentInstance), *GetNameSafe(EquipmentDefinition));
+}
+
+
+
+/**
+ * FEquipmentList
+ */
+UEquipmentInstance* FEquipmentList::AddEntry(UEquipmentDefinition* EquipmentDefinition)
+{
+	UEquipmentInstance* NewEquipment = nullptr;
+
+	check(EquipmentDefinition != nullptr);
+	check(OwningComponent);
+
+	AActor* OwningActor = OwningComponent->GetOwner();
+	check(OwningActor->HasAuthority());
+
+	// Add a new entry for the new equipment.
+	FEquipmentListEntry& NewEntry = Entries.AddDefaulted_GetRef();
+	NewEntry.EquipmentDefinition = EquipmentDefinition;
+	NewEntry.EquipmentInstance = NewObject<UEquipmentInstance>(OwningComponent->GetOwner(), EquipmentDefinition->EquipmentInstanceClass); // Outer object is the owning actor because equipment instances only exist while equipped.
+	NewEquipment = NewEntry.EquipmentInstance;
+
+	// Grant the ability sets that this equipment grants when equipped.
+	if (UCrashAbilitySystemComponent* CrashASC = UCrashAbilitySystemGlobals::GetCrashAbilitySystemComponentFromActor(OwningActor))
+	{
+		for (const TObjectPtr<const UCrashAbilitySet>& AbilitySet : EquipmentDefinition->AbilitySetsToGrant)
+		{
+			AbilitySet->GiveToAbilitySystem(CrashASC, &NewEntry.GrantedAbilitySetHandles, NewEquipment);
+		}
+	}
+	else if (EquipmentDefinition->AbilitySetsToGrant.Num()) // Only warn if this equipment is supposed to grant ability sets.
+	{
+		UE_LOG(LogEquipment, Warning, TEXT("Equipped equipment [%s] on actor [%s] but failed to grant the equipment's ability sets: could not find an ASC for the equipping actor."), *GetNameSafe(EquipmentDefinition), *GetNameSafe(OwningActor));
+	}
+
+	// Determine which equipment skin will be used to spawn the new equipment's actors.
+	UEquipmentSkin* EquipmentSkinToSpawn = nullptr;
+
+	// TODO: Retrieve equipping player's equipment skin.
+
+	// If the player does not have a skin for this equipment, use the equipment's default skin.
+	if (EquipmentSkinToSpawn == nullptr)
+	{
+		EquipmentSkinToSpawn = EquipmentDefinition->DefaultEquipmentSkin;
+	}
+
+	checkf(EquipmentSkinToSpawn, TEXT("Tried to equip equipment [%s], but it does not have a default equipment skin."), *GetNameSafe(EquipmentDefinition));
+
+	// Spawn the new equipment's first- and third-person equipment actors.
+	NewEquipment->SpawnEquipmentActors(EquipmentSkinToSpawn->FirstPersonActorsToSpawn, EEquipmentPerspective::FirstPerson);
+	NewEquipment->SpawnEquipmentActors(EquipmentSkinToSpawn->ThirdPersonActorsToSpawn, EEquipmentPerspective::ThirdPerson);
+
+	// Mark the new entry for replication.
+	MarkItemDirty(NewEntry);
+
+	return NewEquipment;
+}
+
+void FEquipmentList::RemoveEntry(UEquipmentInstance* EquipmentInstance)
+{
+	check(EquipmentInstance != nullptr);
+	check(OwningComponent);
+
+	AActor* OwningActor = OwningComponent->GetOwner();
+	check(OwningActor->HasAuthority());
+
+	// Find the equipment to remove.
+	for (auto EntryIt = Entries.CreateIterator(); EntryIt; ++EntryIt)
+	{
+		FEquipmentListEntry& Entry = *EntryIt;
+		if (Entry.EquipmentInstance == EquipmentInstance)
+		{
+			// Remove any granted ability sets when this equipment is unequipped.
+			if (UCrashAbilitySystemComponent* CrashASC = UCrashAbilitySystemGlobals::GetCrashAbilitySystemComponentFromActor(OwningActor))
+			{
+				Entry.GrantedAbilitySetHandles.RemoveFromAbilitySystem(CrashASC);
+			}
+
+			// Destroy the equipment's spawned actors.
+			EquipmentInstance->DestroyEquipmentActors();
+
+			EntryIt.RemoveCurrent();
+			MarkArrayDirty();
+		}
+	}
+}
+
+TArray<UEquipmentInstance*> FEquipmentList::GetAllEquipment() const
+{
+	// Alloc space for each equipped equipment.
+	TArray<UEquipmentInstance*> Equipment;
+	Equipment.Reserve(Entries.Num());
+
+	// Collect the equipment instances from each equipment entry.
+	for (const FEquipmentListEntry& Entry : Entries)
+	{
+		if (Entry.EquipmentInstance != nullptr)
+		{
+			Equipment.Add(Entry.EquipmentInstance);
+		}
+	}
+
+	return Equipment;
+}
+
+void FEquipmentList::PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize)
+{
+	// Notify the removed equipment that it was unequipped on the client.
+	for (int32 Index : RemovedIndices)
+	{
+		const FEquipmentListEntry& Entry = Entries[Index];
+		if (Entry.EquipmentInstance != nullptr)
+		{
+			Entry.EquipmentInstance->OnUnequipped();
+		}
+	}
+}
+
+void FEquipmentList::PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize)
+{
+	// Notify the added equipment that it was equipped on the client.
+	for (int32 Index : AddedIndices)
+	{
+		const FEquipmentListEntry& Entry = Entries[Index];
+		if (Entry.EquipmentInstance != nullptr)
+		{
+			Entry.EquipmentInstance->OnEquipped();
+		}
+	}
+}
--- Source/ProjectCrash/Equipment/EquipmentList.h
+++ Source/ProjectCrash/Equipment/EquipmentList.h
@@ -2,3 +2,142 @@
 
 #pragma once
 
+#include "AbilitySystem/Abilities/CrashAbilitySet.h"
+#include "Net/Serialization/FastArraySerializer.h"
+#include "EquipmentList.generated.h"
+
+class UEquipmentComponent;
+class UEquipmentDefinition;
+class UEquipmentInstance;
+struct FEquipmentList;
+
+/**
+ * A single piece or collection of equipment. Represents an equipment instance.
+ */
+USTRUCT(BlueprintType)
+struct FEquipmentListEntry : public FFastArraySerializerItem
+{
+	GENERATED_BODY()
+
+	friend FEquipmentList;
+	friend UEquipmentComponent;
+
+public:
+
+	/** Default constructor. */
+	FEquipmentListEntry()
+	{}
+
+	/** Formats this entry as "Equipment Instance (Equipment Definition)". */
+	FString GetDebugString() const;
+
+private:
+
+	/** The equipment of which this entry is an instance. */
+	UPROPERTY()
+	TObjectPtr<UEquipmentDefinition> EquipmentDefinition;
+
+	/** The actual equipment represented by this equipment entry. */
+	UPROPERTY()
+	TObjectPtr<UEquipmentInstance> EquipmentInstance = nullptr;
+
+	/** Handles for the ability sets currently granted by this equipment. Only valid on authority. */
+	UPROPERTY(NotReplicated)
+	FCrashAbilitySet_GrantedHandles GrantedAbilitySetHandles;
+};
+
+
+
+/**
+ * A list of equipment currently equipped by a pawn.
+ */
+USTRUCT(BlueprintType)
+struct FEquipmentList : public FFastArraySerializer
+{
+	GENERATED_BODY()
+
+	friend UEquipmentComponent;
+
+	// Construction.
+
+public:
+
+	/** Default constructor. */
+	FEquipmentList()
+		: OwningComponent(nullptr)
+	{}
+
+	/** Constructor initializing this equipment's owning component. */
+	FEquipmentList(UActorComponent* InOwningComponent)
+		: OwningComponent(InOwningComponent)
+	{}
+
+
+
+	// Equipment management.
+
+public:
+
+	/** Spawns and equips a new instance of the given equipment. */
+	UEquipmentInstance* AddEntry(UEquipmentDefinition* EquipmentDefinition);
+
+	/** Unequips the specified equipment. */
+	void RemoveEntry(UEquipmentInstance* EquipmentInstance);
+
+	/** Returns all currently equipped equipment. */
+	TArray<UEquipmentInstance*> GetAllEquipment() const;
+
+
+
+	// Replication.
+
+/* OnReps. These are only called on clients. The equipment component's EquipItem/UnequipItem functions handle
+ * server-side logic. */
+public:
+
+	/** Fires the OnUnequipped event on clients when equipment is unequipped. */
+	void PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize);
+
+	/** Fires the OnEquipped event on clients when equipment is equipped. */
+	void PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize);
+
+	/** Not used, but required for serializer to bind to these functions correctly. */
+	void PostReplicatedChange(const TArrayView<int32> ChangedIndices, int32 FinalSize) {}
+
+// Serialization.
+public:
+
+	/** Performs data serialization on this serializer's items. */
+	bool NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParams)
+	{
+		return FFastArraySerializer::FastArrayDeltaSerialize<FEquipmentListEntry, FEquipmentList>(Entries, DeltaParams, *this);
+	}
+
+
+
+	// Internals.
+
+private:
+
+	/** The list of equipped equipment. */
+	UPROPERTY()
+	TArray<FEquipmentListEntry> Entries;
+
+	/** The actor component to which this equipment belongs. Should be of type EquipmentComponent. */
+	UPROPERTY(NotReplicated)
+	TObjectPtr<UActorComponent> OwningComponent;
+};
+
+
+
+/**
+ * Struct required for fast array serialization.
+ */
+template<>
+struct TStructOpsTypeTraits<FEquipmentList> : public TStructOpsTypeTraitsBase2<FEquipmentList>
+{
+	enum
+	{
+		WithNetDeltaSerializer = true
+	};
+};
