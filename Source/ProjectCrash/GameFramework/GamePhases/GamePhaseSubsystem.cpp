28fab9d4ef803b8ff0f31e34a7b8fd0747e57b5c
Sam Reitich <sam.reitich@gmail.com>
2024-10-24 13:59:18 -0700

    Implemented game phase subsystem helper functions
    Implemented wrappers and helpers for the game phase subsystem.

--- Source/ProjectCrash/GameFramework/GamePhases/GamePhaseSubsystem.cpp
+++ Source/ProjectCrash/GameFramework/GamePhases/GamePhaseSubsystem.cpp
@@ -2,27 +2,45 @@
 
 #include "GameFramework/GamePhases/GamePhaseSubsystem.h"
 
+#include "AbilitySystem/Components/CrashAbilitySystemComponent.h"
+#include "GameFramework/CrashLogging.h"
+#include "GameFramework/GameStateBase.h"
+#include "GamePhaseAbility.h"
+
 UGamePhaseSubsystem::UGamePhaseSubsystem()
 {
 }
 
-void UGamePhaseSubsystem::PostInitialize()
-{
-	Super::PostInitialize();
-}
-
-bool UGamePhaseSubsystem::ShouldCreateSubsystem(UObject* Outer) const
-{
-	return Super::ShouldCreateSubsystem(Outer);
-}
-
 bool UGamePhaseSubsystem::DoesSupportWorldType(const EWorldType::Type WorldType) const
 {
-	return Super::DoesSupportWorldType(WorldType);
+	return (WorldType == EWorldType::Game || WorldType == EWorldType::PIE);
 }
 
-void UGamePhaseSubsystem::StartPhase(TSubclassOf<UGamePhaseAbility> UGamePhaseAbility, FGamePhaseSignature PhaseEndedCallback)
+void UGamePhaseSubsystem::StartPhase(TSubclassOf<UGamePhaseAbility> PhaseAbility, FGamePhaseSignature PhaseEndedCallback)
 {
+	check(PhaseAbility);
+	UWorld* World = GetWorld();
+	UCrashAbilitySystemComponent* GameStateASC = World->GetGameState()->FindComponentByClass<UCrashAbilitySystemComponent>();
+	if (ensure(GameStateASC))
+	{
+		// Try to activate (start) the new phase.
+		FGameplayAbilitySpec PhaseSpec(PhaseAbility, 1, 0, this);
+		FGameplayAbilitySpecHandle SpecHandle = GameStateASC->GiveAbilityAndActivateOnce(PhaseSpec);
+		FGameplayAbilitySpec* NewPhase = GameStateASC->FindAbilitySpecFromHandle(SpecHandle);
+
+		// Register the active phase if it was successfully started.
+		if (NewPhase && NewPhase->IsActive())
+		{
+			FGamePhaseEntry& Entry = ActivePhases.FindOrAdd(SpecHandle);
+			Entry.PhaseEndedCallback = PhaseEndedCallback;
+		}
+		// Phase failed to start.
+		else
+		{
+			UE_LOG(LogGamePhase, Error, TEXT("Failed to start phase [%s]. Phase ability: [%s]."), *PhaseAbility->GetDefaultObject<UGamePhaseAbility>()->GetGamePhaseTag().ToString(), *GetNameSafe(PhaseAbility));
+			PhaseEndedCallback.ExecuteIfBound(nullptr);
+		}
+	}
 }
 
 void UGamePhaseSubsystem::WhenPhaseStartsOrIsActive(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, const FGamePhaseTagSignature& WhenPhaseActive)
@@ -38,16 +56,45 @@ bool UGamePhaseSubsystem::IsPhaseActive(const FGameplayTag& PhaseTag) const
 	return false;
 }
 
-void UGamePhaseSubsystem::K2_StartPhase(TSubclassOf<UGamePhaseAbility> Phase, const FGamePhaseDynamicSignature& PhaseEnded)
+void UGamePhaseSubsystem::K2_StartPhase(TSubclassOf<UGamePhaseAbility> PhaseAbility, const FGamePhaseDynamicSignature& OnPhaseEnded)
 {
+	if (!ensureAlwaysMsgf(PhaseAbility, TEXT("Failed to start game phase: invalid phase ability given.")))
+	{
+		return;
+	}
+
+	// Create a delegate that fires the BP node's callback pin when the new phase ends.
+	const FGamePhaseSignature EndedDelegate = FGamePhaseSignature::CreateWeakLambda(const_cast<UObject*>(OnPhaseEnded.GetUObject()),
+		[OnPhaseEnded](const UGamePhaseAbility* PhaseAbility)
+		{
+			OnPhaseEnded.ExecuteIfBound(PhaseAbility);
+		});
+
+	StartPhase(PhaseAbility, EndedDelegate);
 }
 
 void UGamePhaseSubsystem::K2_WhenPhaseStartsOrIsActive(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, FGamePhaseTagDynamicSignature WhenPhaseActive)
 {
+	// Wrap the BP node's callback pin in a delegate that can be registered as an observer.
+	const FGamePhaseTagSignature ActiveDelegate = FGamePhaseTagSignature::CreateWeakLambda(WhenPhaseActive.GetUObject(),
+		[WhenPhaseActive](const FGameplayTag& PhaseTag)
+		{
+			WhenPhaseActive.ExecuteIfBound(PhaseTag);
+		});
+
+	WhenPhaseStartsOrIsActive(PhaseTag, MatchType, ActiveDelegate);
 }
 
-void UGamePhaseSubsystem::K2_WhenPhaseEnds(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, FGamePhaseTagDynamicSignature WhenPhaseEnds)
+void UGamePhaseSubsystem::K2_WhenPhaseEnds(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, FGamePhaseTagDynamicSignature OnPhaseEnded)
 {
+	// Wrap the BP node's callback pin in a delegate that can be registered as an observer.
+	const FGamePhaseTagSignature EndedDelegate = FGamePhaseTagSignature::CreateWeakLambda(OnPhaseEnded.GetUObject(),
+		[OnPhaseEnded](const FGameplayTag& PhaseTag)
+		{
+			OnPhaseEnded.ExecuteIfBound(PhaseTag);
+		});
+
+	WhenPhaseEnds(PhaseTag, MatchType, EndedDelegate);
 }
 
 void UGamePhaseSubsystem::OnBeginPhase(const UGamePhaseAbility* PhaseAbility, const FGameplayAbilitySpecHandle PhaseAbilityHandle)
--- Source/ProjectCrash/GameFramework/GamePhases/GamePhaseSubsystem.h
+++ Source/ProjectCrash/GameFramework/GamePhases/GamePhaseSubsystem.h
@@ -10,21 +10,37 @@
 
 class UGamePhaseAbility;
 
+/** Delegate signature for game phase ability callbacks. */
 DECLARE_DELEGATE_OneParam(FGamePhaseSignature, const UGamePhaseAbility* Phase);
 DECLARE_DYNAMIC_DELEGATE_OneParam(FGamePhaseDynamicSignature, const UGamePhaseAbility*, Phase);
 
+/** Delegate signature for game phase tag callbacks. */
 DECLARE_DELEGATE_OneParam(FGamePhaseTagSignature, const FGameplayTag& PhaseTag);
 DECLARE_DYNAMIC_DELEGATE_OneParam(FGamePhaseTagDynamicSignature, const FGameplayTag&, PhaseTag);
 
+/** Match rule for game phase tags. */
 UENUM(BlueprintType)
 enum class EPhaseTagMatchType : uint8
 {
+	// An exact match will only receive messages with exactly the same channel
+	// (e.g., registering for "A.B" will match a broadcast of A.B but not A.B.C)
 	ExactMatch,
+
+	// A partial match will receive any messages rooted in the same channel
+	// (e.g., registering for "A.B" will match a broadcast of A.B as well as A.B.C)
 	PartialMatch
 };
 
 /**
- * 
+ * Manages gameplay flow via "game phases." Active game phases are represented by a Game Phase Ability in the game
+ * state. Game phases are identified via tags: PreGame.CharacterSelection, Game.WarmUp, Game.Playing, etc.
+ *
+ * Parent and child phases can be active at the same time, but not sibling phases. Activating a new phase automatically
+ * ends any phases that are not parents of the new phase.
+ *
+ * TODO: Unbind callbacks from WhenPhaseStartsOrIsActive and WhenPhaseEnds.
+ *
+ * @see UGamePhaseAbility
  */
 UCLASS()
 class PROJECTCRASH_API UGamePhaseSubsystem : public UWorldSubsystem
@@ -37,14 +53,12 @@ class PROJECTCRASH_API UGamePhaseSubsystem : public UWorldSubsystem
 
 public:
 
+	/** Default constructor. */
 	UGamePhaseSubsystem();
-
-	virtual void PostInitialize() override;
-
-	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
 
 protected:
 
+	/** Restricts this subsystem to game and PIE worlds. */
 	virtual bool DoesSupportWorldType(const EWorldType::Type WorldType) const override;
 
 
@@ -53,27 +67,38 @@ protected:
 
 public:
 
-	void StartPhase(TSubclassOf<UGamePhaseAbility> UGamePhaseAbility, FGamePhaseSignature PhaseEndedCallback = FGamePhaseSignature());
+	/** Starts the given phase by activating the ability on the game state. Ends all non-parent phases. */
+	void StartPhase(TSubclassOf<UGamePhaseAbility> PhaseAbility, FGamePhaseSignature PhaseEndedCallback = FGamePhaseSignature());
 
+	/** Binds the given delegate to when a phase with the specified tag starts. Immediately fires if the phase is already
+	 * active. */
 	void WhenPhaseStartsOrIsActive(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, const FGamePhaseTagSignature& WhenPhaseActive);
 
+	/** Binds the given delegate to when a phase with the specified tag ends. */
 	void WhenPhaseEnds(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, const FGamePhaseTagSignature& WhenPhaseEnds);
 
+	/** Returns whether a phase with the specified tag is currently active. */
 	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, BlueprintPure = "false", meta = (AutoCreateRefTerm = "PhaseTag"))
 	bool IsPhaseActive(const FGameplayTag& PhaseTag) const;
 
 protected:
 
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Game Phase", DisplayName = "Start Phase", meta = (AutoCreateRefTerm = "PhaseEnded"))
-	void K2_StartPhase(TSubclassOf<UGamePhaseAbility> Phase, const FGamePhaseDynamicSignature& PhaseEnded);
+	/** Blueprint wrapper for StartPhase. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Game Phase", DisplayName = "Start Phase", meta = (AutoCreateRefTerm = "PhaseEnded", ToolTip = "Starts the given phase by activating the ability on the game state. Ends all non-parent phases."))
+	void K2_StartPhase(TSubclassOf<UGamePhaseAbility> PhaseAbility, const FGamePhaseDynamicSignature& OnPhaseEnded);
 
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Game Phase", DisplayName = "When Phase Starts or Is Active", meta = (AutoCreateRefTerm = "WhenPhaseActive"))
+	/** Blueprint wrapper for WhenPhaseStartsOrIsActive. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Game Phase", DisplayName = "When Phase Starts or Is Active", meta = (AutoCreateRefTerm = "WhenPhaseActive", ToolTip = "Binds the given delegate to when a phase with the specified tag starts. Immediately fires if the phase is already active."))
 	void K2_WhenPhaseStartsOrIsActive(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, FGamePhaseTagDynamicSignature WhenPhaseActive);
 
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Game Phase", DisplayName = "When Phase Ends", meta = (AutoCreateRefTerm = "WhenPhaseEnds"))
+	/** Blueprint wrapper for WhenPhaseEnds. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Game Phase", DisplayName = "When Phase Ends", meta = (AutoCreateRefTerm = "WhenPhaseEnds", ToolTip = "Binds the given delegate to when a phase with the specified tag ends."))
 	void K2_WhenPhaseEnds(FGameplayTag PhaseTag, EPhaseTagMatchType MatchType, FGamePhaseTagDynamicSignature WhenPhaseEnds);
 
+	/** Handles ending non-parent phases and executing callbacks when a new phase begins. */
 	void OnBeginPhase(const UGamePhaseAbility* PhaseAbility, const FGameplayAbilitySpecHandle PhaseAbilityHandle);
+
+	/** Fires relevant callbacks when a phase ends. */
 	void OnEndPhase(const UGamePhaseAbility* PhaseAbility, const FGameplayAbilitySpecHandle PhaseAbilityHandle);
 
 
@@ -83,21 +108,22 @@ protected:
 // Phase tracking.
 private:
 
+	/** Represents an active phase. */
 	struct FGamePhaseEntry
 	{
-	public:
 		FGameplayTag PhaseTag;
 		FGamePhaseSignature PhaseEndedCallback;
 	};
 
+	/** Tracks runtime data for active phase abilities. */
 	TMap<FGameplayAbilitySpecHandle, FGamePhaseEntry> ActivePhases;
 
 // Observers.
 private:
 
+	/** Represents a listener registered to a phase transition callback. */
 	struct FPhaseObserver
 	{
-	public:
 		bool IsMatch(const FGameplayTag& OtherPhaseTag) const;
 
 		FGameplayTag PhaseTag;
@@ -105,6 +131,9 @@ private:
 		FGamePhaseTagSignature PhaseCallback;
 	};
 
+	/** Observers listening for the start of a phase. */
 	TArray<FPhaseObserver> PhaseStartObservers;
+
+	/** Observers listening for the end of a phase. */
 	TArray<FPhaseObserver> PhaseEndObservers;
 };
