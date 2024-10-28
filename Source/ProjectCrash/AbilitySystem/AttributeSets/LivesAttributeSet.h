463b4e0b5c96f2214f9b04ef0db306ca9a66ca72
Sam Reitich <sam.reitich@gmail.com>
2024-10-28 15:14:13 -0700

    Implemented outline for "Lives" attribute set
    We may or may not end up using this attribute set to track player lives, as opposed to using team tags. This is a lot more complex, but also a lot safer, and will probably make scripting observers (e.g. HUD info) a little easier.

--- Source/ProjectCrash/AbilitySystem/AttributeSets/LivesAttributeSet.h
+++ Source/ProjectCrash/AbilitySystem/AttributeSets/LivesAttributeSet.h
@@ -2,18 +2,54 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AbilitySystem/AttributeSets/CrashAttributeSet.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+#include "GameFramework/Messages/CrashVerbMessage.h"
 #include "LivesAttributeSet.generated.h"
 
 /**
  * Handles players' "lives." A player loses a life when they die. When a player loses all of their lives, they can no
- * longer respawn, and usually enter a spectating state.
+ * longer respawn, and usually enter a spectating state. By default, lives are shared by all players on a team.
  *
  * Lives are not used in all game modes. In most game modes that use lives, players lose the game upon running out.
  */
-UCLASS()
+UCLASS(BlueprintType)
 class PROJECTCRASH_API ULivesAttributeSet : public UCrashAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+
+	ULivesAttributeSet();
+
+	virtual void PostInitProperties() override;
+
+protected:
+
+	/** Clamps */
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const override;
+
+private:
+
+	FGameplayMessageListenerHandle DeathListener;
+
+	UFUNCTION()
+	void OnPlayerDeath(FGameplayTag Channel, const FCrashVerbMessage& Message);
+
+public:
+
+	ATTRIBUTE_ACCESSORS(ULivesAttributeSet, Lives);
+
+private:
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Lives, Category = "Ability|Attribute|GameMode", Meta = (AllowPrivateAccess = "true"))
+	FGameplayAttributeData Lives;
+
+protected:
+
+	UFUNCTION()
+	void OnRep_Lives(const FGameplayAttributeData& OldValue);
 };
